423a43c977ff8e695f91844b33be1260c9c247a5
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-03-31 23:43:10 +1030

    Ajax function created to get weather data using api.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 $(document).ready(function () {
     var m = moment().format("MMM, YYYY Do");
     $(".date").text(m);
-    $("#search").on("click", function () {
-        var cityName = $("#input").val ();
-        console.log(cityName);
-        $(".city").text(cityName + " |   ");
-    });
-});
+   
 
-var apiKey = "6db15ea629a8fe04cc16aeecc303ade4";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=adelaide&appid=6db15ea629a8fe04cc16aeecc303ade4";
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (data) {
+            console.log(data);
+
+            $(".temp").text("Temperature: " + data.main.temp + " ˚F");
+            $(".windSpeed").text("Wind Speed: " + data.wind.speed);
+            $(".humidity").text("Humidity: " + data.main.humidity);
+            $(".weatherCondition").text("Weather: " + data.weather[0].main);
+
+            var iconImg = data.weather[0].icon;
+            var weatherIconSrc = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+            var weatherImage = $("<img>");
+            weatherImage.attr("src", weatherIconSrc);
+            $(".icon").append(weatherImage);
+            $(".icon-img img").attr("src", weatherIconSrc);
+
+            
+        });
+    });
