04326b80bff1f4857f21e61799304ab2b0a855fa
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-04-01 16:47:36 +1030

    Created Local Storage for the cities weather data.

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ btnSearch.addEventListener("click", function () {
 
     city.textContent = cityName + " |   ";
 
-    
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=6db15ea629a8fe04cc16aeecc303ade4";
     var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=5&units=imperial&appid=6db15ea629a8fe04cc16aeecc303ade4";
 
@@ -58,6 +58,11 @@ btnSearch.addEventListener("click", function () {
         weatherImage.setAttribute('src', weatherIconSrc);
         icon.appendChild(weatherImage);
 
+        localStorage.setItem("cityStorage", cityName);
+        var cityStorageSearch = localStorage.getItem("cityStorage");
+
+        var searchHistory = "<button id='searchStored'>" + cityStorageSearch + "</button>";
+        $(".cityList").append("<li class = 'list-group-item'>" + searchHistory + "</li>");
 
     });
 
@@ -66,21 +71,81 @@ btnSearch.addEventListener("click", function () {
         method: "GET"
     }).then(function (data) {
         console.log(data);
-
-        var temp1 = document.querySelector(".temp1");
-        temp1.textContent = "Temperature: " + data.list[0].main.temp + " ˚F";
-        var wind1 = document.querySelector(".windSpeed1");
-        wind1.textContent = "Wind Speed: " + data.list[0].wind.speed + " MPH";
+        // First Day Forecast
+        var temp = document.querySelector(".temp1");
+        temp.textContent = "Temperature: " + data.list[0].main.temp + " ˚F";
+        var wind = document.querySelector(".windSpeed1");
+        wind.textContent = "Wind Speed: " + data.list[0].wind.speed + " MPH";
         var hum = document.querySelector(".humidity1");
         hum.textContent = "Humidity: " + data.list[0].main.humidity + "%";
         var weather = document.querySelector(".weatherCondition1");
         weather.textContent = "Weather: " + data.list[0].weather[0].main;
 
-        var icon1 = document.querySelector(".icon1");
+        var icon = document.querySelector(".icon1");
         var weatherIconSrc1 = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
         var weatherImage1 = document.createElement("img");
         weatherImage1.setAttribute("src", weatherIconSrc1);
-        icon1.appendChild(weatherImage1);
+        icon.appendChild(weatherImage1);
+        // Second Day Forecast
+        var temp = document.querySelector(".temp2");
+        temp.textContent = "Temperature: " + data.list[1].main.temp + " ˚F";
+        var wind = document.querySelector(".windSpeed2");
+        wind.textContent = "Wind Speed: " + data.list[1].wind.speed + " MPH";
+        var hum = document.querySelector(".humidity2");
+        hum.textContent = "Humidity: " + data.list[1].main.humidity + "%";
+        var weather = document.querySelector(".weatherCondition2");
+        weather.textContent = "Weather: " + data.list[1].weather[0].main;
+
+        var icon = document.querySelector(".icon2");
+        var weatherIconSrc2 = "https://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + ".png";
+        var weatherImage2 = document.createElement("img");
+        weatherImage2.setAttribute("src", weatherIconSrc2);
+        icon.appendChild(weatherImage2);
+        // Third Day Forecast
+        var temp = document.querySelector(".temp3");
+        temp.textContent = "Temperature: " + data.list[2].main.temp + " ˚F";
+        var wind = document.querySelector(".windSpeed3");
+        wind.textContent = "Wind Speed: " + data.list[2].wind.speed + " MPH";
+        var hum = document.querySelector(".humidity3");
+        hum.textContent = "Humidity: " + data.list[2].main.humidity + "%";
+        var weather = document.querySelector(".weatherCondition3");
+        weather.textContent = "Weather: " + data.list[2].weather[0].main;
+
+        var icon = document.querySelector(".icon3");
+        var weatherIconSrc3 = "https://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + ".png";
+        var weatherImage3 = document.createElement("img");
+        weatherImage3.setAttribute("src", weatherIconSrc3);
+        icon.appendChild(weatherImage3);
+        // Fourth Day Forecast
+        var temp = document.querySelector(".temp4");
+        temp.textContent = "Temperature: " + data.list[3].main.temp + " ˚F";
+        var wind = document.querySelector(".windSpeed4");
+        wind.textContent = "Wind Speed: " + data.list[3].wind.speed + " MPH";
+        var hum = document.querySelector(".humidity4");
+        hum.textContent = "Humidity: " + data.list[3].main.humidity + "%";
+        var weather = document.querySelector(".weatherCondition4");
+        weather.textContent = "Weather: " + data.list[3].weather[0].main;
+
+        var icon = document.querySelector(".icon4");
+        var weatherIconSrc4 = "https://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png";
+        var weatherImage4 = document.createElement("img");
+        weatherImage4.setAttribute("src", weatherIconSrc4);
+        icon.appendChild(weatherImage4);
+        // Fifth Day Forecast
+        var temp = document.querySelector(".temp5");
+        temp.textContent = "Temperature: " + data.list[4].main.temp + " ˚F";
+        var wind = document.querySelector(".windSpeed5");
+        wind.textContent = "Wind Speed: " + data.list[4].wind.speed + " MPH";
+        var hum = document.querySelector(".humidity5");
+        hum.textContent = "Humidity: " + data.list[4].main.humidity + "%";
+        var weather = document.querySelector(".weatherCondition5");
+        weather.textContent = "Weather: " + data.list[4].weather[0].main;
+
+        var icon = document.querySelector(".icon5");
+        var weatherIconSrc5 = "https://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + ".png";
+        var weatherImage5 = document.createElement("img");
+        weatherImage5.setAttribute("src", weatherIconSrc5);
+        icon.appendChild(weatherImage5);
     });
 });
 
